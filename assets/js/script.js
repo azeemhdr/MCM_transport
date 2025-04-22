bf393cef7cb209b44ead626ba9ffc9e5afdaede8
azeemhdr <comazeem@gmail.com>
2025-04-22 13:32:47 +0530

    project section responsive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
   slidesPerView: 4,
-  spaceBetween: 10,
+  spaceBetween: 30,
   a11y: {
     prevSlideMessage: "Previous slide",
     nextSlideMessage: "Next slide",
@@ -11,10 +11,31 @@ const swiper = new Swiper(".swiper", {
   autoplay: {
 	delay: 3000,
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768:{
+      slidesPerView:3,
+      spaceBetween:30,
+    },
+    992:{
+      slidesPerView:4,
+      spaceBetween:30,
+    }
+},
 });
